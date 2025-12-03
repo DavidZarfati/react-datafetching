0daf6e96b6891735ef7f96059ee1fb746a064cb3
DavidZarfati <zarda2001@gmail.com>
2025-12-03 11:41:57 +0100

    feat:test postman

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
 
   let [dati, setDati] = useState([]);
 
-  console.log("Daccio partire chiamata all'api");
-
-  axios
-    .get(
-      "https://flynn.boolean.careers/exercises/api/array/integers?min=1&max=100&items=10"
-    )
-    .then((resp) => {
-      setDati(resp.data.response);
-      console.log(dati);
-    });
-
+  useEffect(() => {
+    console.log("Daccio partire chiamata all'api");
+    axios
+      .get(
+        "https://flynn.boolean.careers/exercises/api/array/integers?min=1&max=100&items=10"
+      )
+      .then((resp) => {
+        setDati(resp.data.response);
+        console.log(dati);
+      });
+  }, [])
   return (
     <>
-      {dati.map((d) => (
-        <div>{d}</div>
+      {dati.map((d, index) => (
+        <div key={index}>{d}</div>
       ))}
     </>
   );
